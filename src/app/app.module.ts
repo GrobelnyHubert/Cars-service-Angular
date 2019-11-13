22fe47a7deb6ddfbb8192a33b15955d114f79b9b
Hubert Grobelny <grobelny.hubert@gmail.com>
2019-11-13 23:31:56 +0100

    Cars service tables with data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CarsModule} from './cars/cars.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CarsModule,
     BrowserModule,
     AppRoutingModule
   ],
